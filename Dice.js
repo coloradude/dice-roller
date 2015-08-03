8bf0e0c5ba94d699bdbb30f50f07d15d8ff6c87f
Stephen Florian <steezeven@gmail.com>
2015-08-03 14:01:44 -0600

    cleaned it up a bit

--- Dice.js
+++ Dice.js
@@ -2,18 +2,29 @@ var diceRoller = function () {
     return Math.floor(Math.random() * 6 + 1);
 }
 
-var diceRollerThree = function () {
+var diceRollerThree = function (numberOfDice) {
     var dice = [];
-    for (var i = 0; i < 3; i++) {
+    for (var i = 0; i < numberOfDice; i++) {
         dice.push(Math.floor(Math.random() * 6 + 1));
     }
     return dice;
 }
 
-var diceRollerToN = function (n) {
+var diceRollerToN = function (numberOfDice, upperBound) {
     var dice = [];
-    for (var i = 0; i < 3; i++) {
-        dice.push(Math.floor(Math.random() * n + 1));
+    for (var i = 0; i < numberOfDice; i++) {
+        dice.push(Math.floor(Math.random() * upperBound + 1));
     }
     return dice;
 }
+
+var diceRollerFour = function (numberOfDice, upperBound) {
+    var dice = [];
+    if (upperBound === undefined) {
+        upperBound = 6;
+    }
+    for (var i = 0; i < numberOfDice; i++) {
+        dice.push(Math.floor(Math.random() * upperBound + 1));
+    }
+    return dice;
+};
